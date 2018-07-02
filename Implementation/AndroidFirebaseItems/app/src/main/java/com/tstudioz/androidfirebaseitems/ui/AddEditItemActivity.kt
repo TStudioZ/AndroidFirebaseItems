cfac7f46fd87755a7d074d4e47eb8b669a3f0106
Tomáš Zahálka <zahalka.t@gmail.com>
2018-07-03 01:40:05 +0200

    Added ValidatedTextInputLayout library, AddEditItemFragment starter layout.

---
+++ Implementation/AndroidFirebaseItems/app/src/main/java/com/tstudioz/androidfirebaseitems/ui/AddEditItemActivity.kt
@@ -0,0 +1,13 @@
+package com.tstudioz.androidfirebaseitems.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.tstudioz.androidfirebaseitems.R
+
+class AddEditItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_item)
+    }
+}
