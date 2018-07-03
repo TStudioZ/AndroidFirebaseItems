ae70345b7c9f8a43f3a44469133642f493ef407a
Tomáš Zahálka <zahalka.t@gmail.com>
2018-07-04 00:25:41 +0200

    Added menu for MainActivity with add item button.

--- Implementation/AndroidFirebaseItems/app/src/main/java/com/tstudioz/androidfirebaseitems/ui/MainActivity.kt
+++ Implementation/AndroidFirebaseItems/app/src/main/java/com/tstudioz/androidfirebaseitems/ui/MainActivity.kt
@@ -2,8 +2,11 @@ package com.tstudioz.androidfirebaseitems.ui
 
 import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.tstudioz.androidfirebaseitems.R
 import com.tstudioz.essentialuilibrary.ui.BaseActivity
+import com.tstudioz.essentialuilibrary.util.ActivityUtils
 
 class MainActivity : BaseActivity(), ItemsFragment.OnFragmentInteractionListener {
 
@@ -14,5 +17,20 @@ class MainActivity : BaseActivity(), ItemsFragment.OnFragmentInteractionListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_add_item -> {
+                ActivityUtils.startActivity(this, AddEditItemActivity::class.java);
+            }
+            else -> return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
