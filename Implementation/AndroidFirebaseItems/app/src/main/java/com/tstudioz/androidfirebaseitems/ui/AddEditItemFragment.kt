91febd7ad4e6bcfaeba3d30533e0ac8770152035
Tomáš Zahálka <zahalka.t@gmail.com>
2018-07-04 00:39:08 +0200

    Add/Edit item menu, added validation on done click.

--- Implementation/AndroidFirebaseItems/app/src/main/java/com/tstudioz/androidfirebaseitems/ui/AddEditItemFragment.kt
+++ Implementation/AndroidFirebaseItems/app/src/main/java/com/tstudioz/androidfirebaseitems/ui/AddEditItemFragment.kt
@@ -3,10 +3,9 @@ package com.tstudioz.androidfirebaseitems.ui
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
+import com.julianraj.validatedtextinputlayout.ValidatedTextInputLayout
 import com.tstudioz.androidfirebaseitems.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -20,9 +19,19 @@ private const val ARG_PARAM2 = "param2"
  */
 class AddEditItemFragment : Fragment() {
 
+    private lateinit var nameInput: ValidatedTextInputLayout;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_add_edit_item, container, false);
+
+        nameInput = view.findViewById(R.id.inputItemName);
 
         TextView(activity).apply {
             setText(R.string.hello_blank_fragment)
@@ -31,5 +40,31 @@ class AddEditItemFragment : Fragment() {
         return view;
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_add_item, menu);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_done -> {
+                submit();
+            }
+            else -> return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    fun submit() {
+        if (!validateFields())
+            return;
+    }
+
+    fun validateFields(): Boolean {
+        var flag = true;
+        if (!nameInput.validate()) {
+            flag = false;
+        }
+        return flag;
+    }
 
 }
